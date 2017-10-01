c7377dd06d83bed85099f402989e2326fd012441
Miguel Jardines <mjard009@fiu.edu>
2017-10-01 01:43:23 -0400

    version 0.0 new logo almost done

--- myapp/app/src/main/java/com/example/migueljardinesjr/myapp/qr_scan.java
+++ myapp/app/src/main/java/com/example/migueljardinesjr/myapp/qr_scan.java
@@ -18,16 +18,20 @@ import java.net.InetAddress;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Button;
 import android.widget.TextView;
 import java.net.UnknownHostException;
 import java.io.IOException;
 import android.support.annotation.NonNull;
+import android.graphics.PorterDuff;
 
 
 public class qr_scan extends Activity {
-    private Button startButton,stopButton;
+    private ImageButton startButton;
+    private boolean recording = false;
 
     public byte[] buffer;
     public static DatagramSocket socket;
@@ -38,7 +42,7 @@ public class qr_scan extends Activity {
     private static final String LOG_TAG = "AudioRecordTest";
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
-    private int sampleRate = 16000 ; // 44100 for music
+    private int sampleRate = 16000; // 44100 for music
     //private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
     private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
     private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
@@ -59,13 +63,26 @@ public class qr_scan extends Activity {
         requestPermissions(permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
 
-        startButton = (Button) findViewById (R.id.start_button);
-        stopButton = (Button) findViewById (R.id.stop_button);
+        startButton = (ImageButton) findViewById (R.id.start_button);
 
-        startButton.setOnClickListener (startListener);
-        stopButton.setOnClickListener (stopListener);
+        startButton.setOnClickListener (handleListener);
+
 
     }
+
+    private final OnClickListener handleListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(!recording){
+                startListener(v);
+            }
+            else {
+                stopListener(v);
+            }
+        }
+
+    };
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -79,30 +96,21 @@ public class qr_scan extends Activity {
 
     }
 
-    private final OnClickListener stopListener = new OnClickListener() {
+    public void stopListener(View v){
+        recording = false;
+        ImageView recordButton = (ImageView)v;
+        recordButton.setColorFilter(getResources().getColor(R.color.shell_blue), PorterDuff.Mode.SRC_IN);
+        status = false;
+        recorder.release();
 
-        @Override
-        public void onClick(View arg0) {
-            TextView statusText = (TextView) findViewById (R.id.status);
-            statusText.setText("Not Recording");
-
-            status = false;
-            recorder.release();
-            Log.d(LOG_TAG,"Recorder released");
-        }
-
+        Log.d(LOG_TAG,"Recorder released");
     };
 
-    private final OnClickListener startListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View arg0) {
-            TextView statusText = (TextView) findViewById (R.id.status);
-            statusText.setText("Recording");
-            status = true;
-            startStreaming();
-        }
-
+    public void startListener(View v){
+        recording = true;
+        ImageView recordButton = (ImageView)v;
+        recordButton.setColorFilter(getResources().getColor(R.color.recordRED), PorterDuff.Mode.SRC_IN);
+        startStreaming();
     };
 
     public void startStreaming() {
@@ -121,9 +129,10 @@ public class qr_scan extends Activity {
 
                     Log.d("VS","Buffer created of size " + minBufSize);
                     DatagramPacket packet;
-
-                    final InetAddress destination = InetAddress.getByName("192.168.56.1");
+                    //"192.168.56.1"
+                    final InetAddress destination = InetAddress.getByName("10.108.120.62");
                     Log.d("VS", "Address retrieved");
+                    Log.d("VS", destination.toString());
 
                     //recorder = findAudioRecord();//new AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,channelConfig,audioFormat,minBufSize*10);
                     recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,channelConfig,audioFormat,minBufSize*100);
@@ -138,9 +147,6 @@ public class qr_scan extends Activity {
                     }
 
 
-                    // Log.d("VS", "Recorder initialized");
-
-
                     recorder.startRecording();
 
 
@@ -152,6 +158,11 @@ public class qr_scan extends Activity {
 
                         //putting buffer in the packet
                         packet = new DatagramPacket (buffer,buffer.length,destination,port);
+
+                        // byteArray = new byte[5];
+                        //byte [] byteArray = {'a', 'T','E','S','T'};
+                        //DatagramPacket packet1 = new DatagramPacket(byteArray,5,destination,port);
+                        //packet1 = byteArray;
 
                         socket.send(packet);
                         System.out.println("MinBufferSize: " +minBufSize);
@@ -175,7 +186,7 @@ public class qr_scan extends Activity {
 
     private int[] mSampleRates = new int[] { 8000, 11025, 22050, 44100 };
     int bufferSize;
-    static final int RECORDINGDURATION = 20;
+    static final int RECORDINGDURATION = 10000;
 
     AudioRecord audioInput = findAudioRecord();
 
